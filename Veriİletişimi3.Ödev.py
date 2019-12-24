61d2897124642a90e20872ee9e46d8daaf594900
nto4 <mehmet94nto@gmail.com>
2019-12-24 21:07:16 +0300

    clean

--- Veriİletişimi3.Ödev.py
+++ Veriİletişimi3.Ödev.py
@@ -1,12 +1,23 @@
 import random
+"""
+This C 7,4 hamming implamentation is following this steps;
+1-get input and print it:                                                         aa
+2-print asci this input:                                                          ['01100001', '01100001']
+3-convert 4 bit elements this each elements:                                      ['0110', '0001', '0110', '0001']
+4-Generate hamming code for each elements and add 3 control bits and print it:    ['1100110', '1101001', '1100110', '1101001'] 
+5-Add error bit each element:                                                     ['1100010', '1100001', '0100110', '1001001']
+6-Check paritys correct errors:                                                   ['0110', '0001', '0110', '0001']
+7-Generate asci bits using two corrected elements and print asci bits:            ['01100001', '01100001']
+8-If same result to input hamming error correction is work:                       aa
 
+"""
 def string2bits(s=''):
     return [bin(ord(x))[2:].zfill(8) for x in s]
 
 def bits2string(b=None):
     return ''.join([chr(int(x, 2)) for x in b])
 
-text = input()
+text = input("C (7,4) Hamming error correction code system \n Type the text to be hamming encoded (max. 100 character) \n")
 fourbitarray = []
 encodedarray = []
 encodedaddedarray = []
@@ -18,30 +29,21 @@ for i in range(0,len(bitarray)):
     fourbitarray.append((bitarray[i])[0:4])
     fourbitarray.append((bitarray[i])[4:9])
 
-print(fourbitarray)
 
-
-def hamcodederdecoder(p):
-    # the encoding matrix
+def hamcodedandAddError(p):
+    #Encoding matrix
     G = ['1101', '1011', '1000', '0111', '0100', '0010', '0001']
-    # the parity-check matrix
-    H = ['1010101', '0110011', '0001111']
-    Ht = ['100', '010', '110', '001', '101', '011', '111']
-    # the decoding matrix
-    R = ['0010000', '0000100', '0000010', '0000001']
     p = str(p)
     x = ''.join([str(bin(int(i, 2) & int(p, 2)).count('1') % 2) for i in G])
     encodedarray.append(x)
-    # add 1 bit error
+    #Add error 1 bit in 7 bit
     e = random.randint(1, 7)
-    x = list(x)
-    x[e - 1] = str(1 - int(x[e - 1]))
-    x = ''.join(x)
+    if e > 0:# İf error exist
+        x = list(x)
+        x[e - 1] = str(1 - int(x[e - 1]))
+        x = ''.join(x)
     encodedaddedarray.append(x)
-testinput = 1101001
-def correction(x):
-    # the encoding matrix
-    G = ['1101', '1011', '1000', '0111', '0100', '0010', '0001']
+def correctionanddecode(x):
     # the parity-check matrix
     H = ['1010101', '0110011', '0001111']
     Ht = ['100', '010', '110', '001', '101', '011', '111']
@@ -52,7 +54,7 @@ def correction(x):
         e = int(Ht[int(z, 2) - 1], 2)
     else:
         e = 0
-     # correct the error
+    #Check parity and fix errored bit
     if e > 0:
         x = list(x)
         x[e - 1] = str(1 - int(x[e - 1]))
@@ -62,12 +64,16 @@ def correction(x):
     correctedfourbitarray.append(p)
 
 for i in range(0,len(fourbitarray)):
-    hamcodederdecoder(fourbitarray[i])
+    hamcodedandAddError(fourbitarray[i])
+
+print(bitarray)
+print(fourbitarray)
+
 print(encodedarray)   # hatabiti eklenmeden kodlanmış hali
 print(encodedaddedarray)#  Hata biti eklenmiş kodlanmış hali
 
 for i in range(0,len(fourbitarray)):
-    correction(str(encodedaddedarray[i]))
+    correctionanddecode(str(encodedaddedarray[i]))
 
 print(correctedfourbitarray)
 
